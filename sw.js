b505980a0b4258fe4aacc91595bc04c5402ef1ff
juanbautistacrespialomar <juanbautistacrespialomar@gmail.com>
2026-08-02 11:44:13 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 //
 // Para publicar: subí el número de CACHE (v6 -> v7 -> ...).
 
-const CACHE = "tuprofe-v18";
+const CACHE = "tuprofe-v19";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
